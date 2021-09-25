3cd01b09026983783d869a17453aa7d6190827c1
Utsav Deep <utsavdeep01@gmail.com>
2021-09-25 23:30:20 +0530

    Fetch news from Hacker news done

---
+++ main.py
@@ -0,0 +1,6 @@
+from prepare_news import PrepareNews
+
+news = PrepareNews()
+
+top_10_news = news.create_news()
+print(top_10_news)
---
+++ prepare_news.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class PrepareNews:
+    def __init__(self):
+        self.response = requests.get("https://news.ycombinator.com/")
+        self.yc_webpage = self.response.text
+        self.article_texts = []
+        self.article_links = []
+        self.create_soup()
+
+    def create_soup(self):
+        soup = BeautifulSoup(self.yc_webpage, "html.parser")
+        self.article_texts = [
+            article_tag.getText()
+            for article_tag in soup.findAll(name="a", class_="storylink")
+        ]
+        self.article_links = [
+            article_tag.get("href")
+            for article_tag in soup.findAll(name="a", class_="storylink")
+        ]
+
+    def create_news(self):
+        list_news = [
+            {
+                "a_text": self.article_texts[index],
+                "a_link": self.article_links[index],
+            }
+            for index in range(10)
+        ]
+        return list_news
